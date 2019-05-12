29e28277226b180f7cc33c240f0a62add3830416
YantCaccia <yantcaccia@gmail.com>
2019-05-12 23:47:24 +0200

    Testing Git from Command Line

--- Ricorsione/List/item.c
+++ Ricorsione/List/item.c
@@ -1,20 +1,20 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include"item.h"
+#include "item.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-Item in_item(){
+Item in_item() {
     Item a = malloc(sizeof(int));
     scanf("%d", a);
     return a;
 }
 
-void out_item(Item a){
+void out_item(Item a) {
     int *b = a;
     printf("%d ", *b);
 }
 
-int comp_items(Item a, Item b){
+int comp_items(Item a, Item b) {
     int *a1 = a;
     int *b1 = b;
     return (*a1 - *b1);
-}
+}  // sto aggiungendo un commento giusto per provare git da terminale
